d31b8494aa38c6639698b2d2cca941b5e5c931f7
Mostafa Saber <sabermostafa940@gmail.com>
2023-03-20 14:33:00 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,10 @@
 /**
  * main - Prints the size of various typesa based on 
  * the computer it is compiled and run on..
- * Return: Always 0 
+ * Return: Always 0.
  */
+
+
 int main(void)
 
 {
@@ -18,3 +20,4 @@ int main(void)
 
 	printf("Size of a float: %zu byte(s)\n", sizeof(float)); 
 	return (0);
+}	
