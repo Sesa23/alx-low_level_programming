7c07bde1871559f3183da517195799784704a424
Mostafa Saber <sabermostafa940@gmail.com>
2023-03-20 15:23:18 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point 
  * 
@@ -11,7 +11,7 @@ int main(void)
 
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" -  Dora Korpar, 2015-10-19"));
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 return (1);
 }
 
